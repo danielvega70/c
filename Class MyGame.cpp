f4c1fb87ceb154a69e45339a205bc782fab79030
danielvega70 <80554408+danielvega70@users.noreply.github.com>
2023-03-06 11:45:49 -0500

    Create Class MyGame.cpp
    Signed-off-by: danielvega70 <80554408+danielvega70@users.noreply.github.com>

---
+++ Class MyGame.cpp
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MyGame : MonoBehaviour 
+{
+    void Start()
+    {
+        // Start is called before the first frame update
+    }
+
+    void Update()
+    {
+        // Update is called once per frame
+    }
+}
+
+public class Paddle : MonoBehaviour
+{
+    void Start()
+    {
+        // Start is called before the first frame update
+    }
+
+    void Update()
+    {
+        // Update is called once per frame
+    }
+}
+
+public class MyTexture : MonoBehaviour
+{
+    void Start()
+    {
+        // Start is called before the first frame update
+    }
+
+    void Update()
+    {
+        // Update is called once per frame
+    }
+}
+
+public class MyCollider : MonoBehaviour
+{
+    void Start()
+    {
+        // Start is called before the first frame update
+    }
+
+    void Update()
+    {
+        // Update is called once per frame
+    }
+}
+
+public class MyRigidbody : MonoBehaviour
+{
+    void Start()
+    {
+        // Start is called before the first frame update
+    }
+
+    void Update()
+    {
+        // Update is called once per frame
+    }
+}
+
+public class MyCharacter : MonoBehaviour
+{
+    void Start()
+    {
+        // Start is called before the first frame update
+    }
+
+    void Update()
+    {
+        // Update is called once per frame
+    }
+}
+
+public class AudioUpdater : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // Update audio source here
+    }
+}
